f66656526a7ca04cb6cfbe1590defa1b5c95cd0c
rmoreno <raul.moreno04@gmail.com>
2021-02-23 21:11:39 +0100

    Cambios en metodo optimizacion

--- src/controllers/L3Fertilicalc/fertilizersCtrl.js
+++ src/controllers/L3Fertilicalc/fertilizersCtrl.js
@@ -1,45 +1,106 @@
 const path = require('path');
 const fs = require('fs');
+const { get } = require('http');
 
 module.exports = function () {
 
-	async function optimization (names, N, P, K) {
+	async function get (names) {
 
-		let fertilizers = JSON.parse(fs.readFileSync(path.join(path.resolve(), 'data', 'fertilicalc-fertilizers-data.json'), 'utf8')),
-			i, amounts;
+		let fertilizers = JSON.parse(fs.readFileSync(path.join(path.resolve(), 'data', 'fertilicalc-fertilizers-data.json'), 'utf8'));
 		names &&
 			(fertilizers = fertilizers.filter(fertilizer => names.includes(fertilizer.name)));
+		return fertilizers;
+	}
 
+	async function pickBest (fertilizers, N, P, K) {
+
+		let i, fertilizer, c_N, c_P, c_K;
+		fertilizers = fertilizers.filter(fertilizer => fertilizer.N_total && fertilizer.P_total && fertilizer.K_total);
 		for (i = 0; i < fertilizers.length && (fertilizer = fertilizers[i]); i++) {
-			amounts = [0];
-			N &&
-				(fertilizer.N_total &&
-					amounts.push(N * 100 / fertilizer.N_total)
-				||
-					amounts.push(Number.MAX_SAFE_INTEGER));
-			P &&
-				(fertilizer.P_total &&
-					amounts.push(P * 100 / fertilizer.P_total)
-				||
-					amounts.push(Number.MAX_SAFE_INTEGER));
-			K &&
-				(fertilizer.K_total &&
-					amounts.push(K * 100 / fertilizer.K_total)
-				||
-					amounts.push(Number.MAX_SAFE_INTEGER));
-
-			fertilizer.amount = Math.max(...amounts);
-			fertilizer.N_total &&
-				(fertilizer.N = fertilizer.amount * fertilizer.N_total / 100);
-			fertilizer.P_total &&
-				(fertilizer.P = fertilizer.amount * fertilizer.P_total / 100);
-			fertilizer.K_total &&
-				(fertilizer.K = fertilizer.amount * fertilizer.K_total / 100);
-			fertilizer.cost = fertilizer.amount * fertilizer.price;
+			c_N = fertilizer.N_total / 100;
+			c_P = fertilizer.P_total / 100;
+			c_K = fertilizer.K_total / 100;
+			fertilizer.amount = (N*c_N + P*c_P + K*c_K) / (c_N*c_N + c_P*c_P + c_K*c_K);
+			fertilizer.N = c_N*fertilizer.amount;
+			fertilizer.P = c_P*fertilizer.amount;
+			fertilizer.K = c_K*fertilizer.amount;
+			fertilizer.cost = fertilizer.amount*fertilizer.price;
 		}
-		return fertilizers.sort((a, b) => a.cost - b.cost);
+		return fertilizers.sort((a, b) => a.cost - b.cost)[0];
 	}
+
+	async function optimize (fertilizers, N, P, K) {
+
+		fertilizers.splice(2, fertilizers.length);
+		if (fertilizers.length < 2) {
+			return pickBest(fertilizers, N, P, K);
+		}
+		let A = [], A_j, k, i, j, c_j, det, fertilizer,
+			tuples = [['P', 'K', 'N'], ['P', 'N', 'K'], ['K', 'N', 'P']],
+			required = {N: N, P: P, K: K};
+		for (k = 0; k < tuples.length; k++) {
+			A = [];
+			for (i = 0; i < tuples[k].length - 1; i++) {
+				A.push([]);
+				for (j = 0; j < 2; j++) {
+					c_j = (fertilizers[j][`${tuples[k][i]}_total`] || 0.0) / 100;
+					A[i].push(c_j);
+				}
+			}
+			if (!(det = determinant2X2(A))) {
+				continue;
+			}
+			for (j = 0; j < fertilizers.length && (fertilizer = fertilizers[j]); j++) {
+				A_j = [[...A[0]], [...A[1]]];
+				A_j[0][j] = required[tuples[k][0]];
+				A_j[1][j] = required[tuples[k][1]];
+				fertilizer.amount = Math.max(determinant2X2(A_j) / det, fertilizer.amount || 0.0);
+			}
+		}
+		for (j = 0; j < fertilizers.length && (fertilizer = fertilizers[j]); j++) {
+			fertilizer.cost = fertilizer.amount*fertilizer.price;
+		}
+		return fertilizers;
+	}
+
 	return {
-		optimization: optimization
+		get: get,
+		pickBest: pickBest,
+		optimize: optimize
 	}
+}
+
+function determinant2X2(A) {
+	if (typeof A != 'object' || A.length != 2) {
+		return null;
+	}
+	for (let i = 0, j; i < 2; i++) {
+		if (typeof A[i] != 'object' || A[i].length != 2) {
+			return null;
+		}
+		for (j = 0; j < 2; j++) {
+			if (!Number.isFinite(A[i][j])) {
+				return null;
+			}
+		}
+	}
+	return A[0][0]*A[1][1] - A[0][1]*A[1][0];
+}
+
+function determinant3X3(A) {
+	if (typeof A != 'object' || A.length != 3) {
+		return null;
+	}
+	for (let i = 0, j; i < 3; i++) {
+		if (typeof A[i] != 'object' || A[i].length != 3) {
+			return null;
+		}
+		for (j = 0; j < 3; j++) {
+			if (!Number.isFinite(A[i][j])) {
+				return null;
+			}
+		}
+	}
+	return A[0][0]*A[1][1]*A[2][2] + A[0][1]*A[1][2]*A[2][0] + A[0][2]*A[1][0]*A[2][1]
+		- (A[2][0]*A[1][1]*A[0][2] + A[2][1]*A[1][2]*A[0][0] + A[2][2]*A[1][0]*A[0][1]);
 }
--- src/routes/fertilizers.js
+++ src/routes/fertilizers.js
@@ -6,17 +6,24 @@ const L3FertilicalcFertilizersCtrl = require('../controllers/L3Fertilicalc/ferti
 
 module.exports = function () {
 
-	router.get('/optimization', asyncHandler(async (req, res) => {
+	router.get('/optimized', asyncHandler(async (req, res) => {
 
-		const response = {'results': []},
-			names = typeof req.query.names === 'string' && req.query.names.split(','),
+		const names = typeof req.query.names === 'string' && req.query.names.split(','),
+			fertilizers = await L3FertilicalcFertilizersCtrl.get(names),
 			N = typeof req.query.N === 'string' && parseFloat(req.query.N) || 0.0,
 			P = typeof req.query.P === 'string' && parseFloat(req.query.P) || 0.0,
 			K = typeof req.query.K === 'string' && parseFloat(req.query.K) || 0.0;
-
-		response.results = await L3FertilicalcFertilizersCtrl.optimization(names, N, P, K);
-	
-		res.json(response);
+		res.json(await L3FertilicalcFertilizersCtrl.optimize(fertilizers, N, P, K));
 	}));
+
+	router.get('/bestone', asyncHandler(async (req, res) => {
+
+		const fertilizers = await L3FertilicalcFertilizersCtrl.get(),
+			N = typeof req.query.N === 'string' && parseFloat(req.query.N) || 0.0,
+			P = typeof req.query.P === 'string' && parseFloat(req.query.P) || 0.0,
+			K = typeof req.query.K === 'string' && parseFloat(req.query.K) || 0.0;
+		res.json(await L3FertilicalcFertilizersCtrl.pickBest(fertilizers, N, P, K));
+	}));
+
 	return router;
 }
