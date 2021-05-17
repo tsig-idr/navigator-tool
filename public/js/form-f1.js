bcca9e7d57a02d4853321b3316d43dfa04499f16
rmoreno <raul.moreno04@gmail.com>
2021-05-17 14:57:09 +0200

    l1 completa

--- public/js/form-f1.js
+++ public/js/form-f1.js
@@ -12,53 +12,56 @@ form.querySelectorAll('button').forEach(function (button) {
 		if (!form.checkValidity()) {
 			return false;
 		}
-		fetch(`/F1/${ev.target.name}`, {
-			method: 'POST',
-			body: JSON.stringify(FormDataJson.formToJson(form)),
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8'
-			}
-		}).then(function (response) {
-			if (response.ok) {
-				return response.json();
-			}
-			return Promise.reject(response);
-		}).then(function (data) {
-			let tr, td,
-				i, name;
-			switch (ev.target.name) {
-				case 'SWB':
-				case 'SNB/weekly':
-					const tr_tmpl = table.querySelector('tr[name="template"]');
-					for (i = 0; i < data.results.length; i++) {
-						tr = tr_tmpl.cloneNode(true);
-						for (name in data.results[i]) {
-							(td = tr.querySelector(`td[name="${name}"]`)) &&
-								(td.innerHTML = 
-									(name == 'Fecha' || name == 'Sem') && 
-										data.results[i][name]
-									||
-										parseFloat(data.results[i][name]).toFixed(2));
+		ev.target.name &&
+			fetch(`/F1/${ev.target.name}`, {
+				method: 'POST',
+				body: JSON.stringify(FormDataJson.formToJson(form)),
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8'
+				}
+			}).then(function (response) {
+				if (response.ok) {
+					return response.json();
+				}
+				return Promise.reject(response);
+			}).then(function (data) {
+				let tr, td, tbody,
+					i, name;
+				switch (ev.target.name) {
+					case 'SWB':
+					case 'SNB/weekly':
+						const tr_tmpl = table.querySelector('tr[name="template"]');
+						table.removeChild(table.querySelector('tbody'));
+						table.appendChild(tbody = document.createElement('tbody'));
+						tbody.appendChild(tr_tmpl);
+						for (i = 0; i < data.results.length; i++) {
+							tr = tr_tmpl.cloneNode(true);
+							for (name in data.results[i]) {
+								(td = tr.querySelector(`td[name="${name}"]`)) &&
+									(td.innerHTML = 
+										(name == 'Fecha' || name == 'Sem') && 
+											data.results[i][name]
+										||
+											parseFloat(data.results[i][name]).toFixed(2));
+							}
+							tbody.appendChild(tr);
 						}
-						table.querySelector('tbody').appendChild(tr);
-					}
-					console.log(data)
-					break;
-				case 'SNB/calendar':
-					let parts;
-					for (name in data.results) {
-						parts = name.split('_');
-						(td = table.querySelector(`tr[name="${parts[0]}"]>td[name="${parts.slice(1).join('_')}"]`)) &&
-							(td.innerHTML = parseFloat(data.results[name]).toFixed(2));
-					}
-					break;
-				default:
-					break;
-			}
-			table.classList.remove('d-none');
-		}).catch(function (error) {
-			console.warn('Something went wrong.', error);
-		});
+						break;
+					case 'SNB/calendar':
+						let parts;
+						for (name in data.results) {
+							parts = name.split('_');
+							(td = table.querySelector(`tr[name="${parts[0]}"]>td[name="${parts.slice(1).join('_')}"]`)) &&
+								(td.innerHTML = parseFloat(data.results[name]).toFixed(2));
+						}
+						break;
+					default:
+						break;
+				}
+				table.classList.remove('d-none');
+			}).catch(function (error) {
+				console.warn('Something went wrong.', error);
+			});
 	});
 });
 
