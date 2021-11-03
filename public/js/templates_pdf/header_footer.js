bbc75a459bd94ed64f858522d5b9897e01b68f8b
jsanc70 <jsanc70@hotmail.com>
2021-11-03 11:29:41 +0100

    Cambios en la cabecera y pie de los PDFs

--- public/js/templates_pdf/header_footer.js
+++ public/js/templates_pdf/header_footer.js
@@ -3,19 +3,16 @@ const addFooters = (doc) => {
     const pageCount = doc.internal.getNumberOfPages();
     doc.setFont('helvetica');
     doc.setFontSize(8);
+    doc.setTextColor(150,150,150);
     const today  = new Date();
     for (let i = 1; i <= pageCount; i++) {
         doc.setPage(i);
-        
-        doc.text(' -  ' + String(i) + ' / ' + String(pageCount) + '  - ', doc.internal.pageSize.width / 2, 287, {
-            align: 'center'
-        });
-
-        doc.text(today.toLocaleString(), doc.internal.pageSize.width -20, 287, {
+        doc.text(' -  ' + String(i) + ' / ' + String(pageCount) + '  - ', doc.internal.pageSize.width -14, doc.internal.pageSize.height - 10, {
             align: 'right'
         });
-
-
+        doc.text(today.toLocaleString(), 14, doc.internal.pageSize.height - 10, {
+            align: 'left'
+        });
     }
 }
 
@@ -26,11 +23,15 @@ const addHeaders = (doc, texto) => {
     for (let i = 1; i <= pageCount; i++) {
         doc.setPage(i);
         doc.addImage("../../img/LogoFastNavigator.jpg", "JPEG", 10, 6, 15, 12);
+        doc.setTextColor(150,150,150);
         doc.text("Navigator Tool", 28, 14);
         doc.setFont('helvetica', 'italic');
         //doc.setFontSize(8);
-        doc.text(' -  ' + texto + '  - ', doc.internal.pageSize.width / 2, 14, {
-            align: 'center'
+        doc.text(texto, doc.internal.pageSize.width -14, 14, {
+            align: 'right'
         });
+        doc.setLineWidth(0.5);
+        doc.setDrawColor(180, 180, 180);
+        doc.line(28, 17, doc.internal.pageSize.width -14, 17);
     }
 }
