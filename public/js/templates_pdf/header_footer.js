43e018486520f9619b2bd32758513a1c6fded008
jsanc70 <jsanc70@hotmail.com>
2021-11-08 13:44:28 +0100

    PDFs: Añadido código de seguridad en pie

--- public/js/templates_pdf/header_footer.js
+++ public/js/templates_pdf/header_footer.js
@@ -1,16 +1,27 @@
 
+const makeId = (n) => {
+    var text = '',
+        possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+        i = 0;
+    for (i; i < n; i += 1) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+// eslint-disable-next-line no-unused-vars
 const addFooters = (doc) => {
     const pageCount = doc.internal.getNumberOfPages();
     doc.setFont('helvetica');
     doc.setFontSize(8);
     doc.setTextColor(150,150,150);
-    const today  = new Date();
+    const tx = 'Chk:' + makeId(10) + ' - ' + (new Date()).toLocaleString();
     for (let i = 1; i <= pageCount; i++) {
         doc.setPage(i);
         doc.text(' -  ' + String(i) + ' / ' + String(pageCount) + '  - ', doc.internal.pageSize.width -14, doc.internal.pageSize.height - 10, {
             align: 'right'
         });
-        doc.text(today.toLocaleString(), 14, doc.internal.pageSize.height - 10, {
+        doc.text(tx, 14, doc.internal.pageSize.height - 10, {
             align: 'left'
         });
     }
