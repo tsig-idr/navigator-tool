26b0bdd136bc74545c667081e1895f89788f768c
raul <raul.moreno04@gmail.com>
2023-11-29 15:58:38 +0100

    siar

--- src/routes/SIAR.js
+++ src/routes/SIAR.js
@@ -4,13 +4,14 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const asyncHandler = require('express-async-handler');
+const multer = require('multer');
 
 module.exports.router = function () {
 
-	router.post('/', asyncHandler(async (req, res) => {
-		const data = typeof req.body == 'object' && req.body || {};
+	router.post('/', multer().single('water'), asyncHandler(async (req, res) => {
+		const json = typeof req.file == 'object'&& req.file.buffer && req.file.buffer.toString() || typeof req.body == 'object' && (req.body.water || JSON.stringify(req.body));
 		let html = fs.readFileSync(path.join(path.resolve(), 'public', 'index4SIAR.html'), 'utf-8');
-		html = html.replace('{id:123}', JSON.stringify(data));
+		html = html.replace('{id:123}', json);
 		res.send(html);
 	}));
 
